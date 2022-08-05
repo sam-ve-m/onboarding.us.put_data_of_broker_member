4f926e3a4dc63850ece610d03ba7953d593cd016
renatamonteiro <rcm@lionx.com.br>
2022-08-05 08:36:36 -0300

    feat refactoring

--- func/src/domain/enums/status_code/enum.py
+++ func/src/domain/enums/status_code/enum.py
@@ -4,14 +4,12 @@ from enum import IntEnum
 
 class InternalCode(IntEnum):
     SUCCESS = 0
-    INVALID_PARAMS = 10
     JWT_INVALID = 30
     INTERNAL_SERVER_ERROR = 100
     NOT_SENT_TO_PERSEPHONE = 60
     INVALID_ONBOARDING_STEP = 49
     TRANSPORT_LAYER_ERROR = 69
     UNIQUE_ID_WAS_NOT_UPDATED = 88
-    HTTP_CONNECTION_POLL = 79
 
     def __repr__(self):
         return self.value
