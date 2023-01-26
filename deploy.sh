d397521560c2204d4849aaf9c98bb56ef54b7f02
Samuel Veloso Marçal <samuel.marcal@ligainvest.com.br>
2023-01-26 10:50:29 -0300

    "build: changed fission strategy to poolmg"

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 fission spec init
-fission env create --spec --name onb-us-broker-mmbr-env --image nexus.sigame.com.br/fission-onboarding-us-broker-member-ben:0.1.0 --poolsize 0 --version 3 --imagepullsecret "nexus-v3" --spec
-fission fn create --spec --name onb-us-broker-mmbr-fn --env onb-us-broker-mmbr-env --code fission.py --targetcpu 80 --executortype newdeploy --maxscale 3 --requestsperpod 10000 --spec
+fission env create --spec --name onb-us-broker-mmbr-env --image nexus.sigame.com.br/fission-onboarding-us-broker-member-ben:0.1.0 --poolsize 2 --graceperiod 3 --version 3 --imagepullsecret "nexus-v3" --spec
+fission fn create --spec --name onb-us-broker-mmbr-fn --env onb-us-broker-mmbr-env --code fission.py --executortype poolmgr --requestsperpod 10000 --spec
 fission route create --spec --name onb-us-broker-mmbr-rt --method PUT --url /onboarding/broker_member --function onb-us-broker-mmbr-fn
